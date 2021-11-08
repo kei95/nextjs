722d183c8afc922f1e4747c598b12dc4c0bd8548
kei95 <kei0613shine.star@gmail.com>
2021-11-07 19:30:00 -0800

    Fix the shameful texts of introduction

--- src/components/Blocks/AboutBlock/index.tsx
+++ src/components/Blocks/AboutBlock/index.tsx
@@ -3,23 +3,26 @@ import { FadeInSection } from "src/components/FadeInSection";
 import { SlashSpace } from "../../SlashSpace";
 import styles from "./AboutBlock.module.css";
 
-export const AboutBlock = ({}): JSX.Element => {
+export const AboutBlock = (): JSX.Element => {
   return (
     <section className={styles.container} id="about">
       <FadeInSection>
         <h2 className={styles.mainLine}>Welcome to Kei's page</h2>
         <p>
-          I'm a software Engineer who loves to inspire and be inspired by
-          beautiful things. The moment when my idea turns to reality is the best
-          moment of my life!
+          I'm a software developer who believes a little idea makes huge impact
+          to the world. I love creative solutions and to be inspired by them.
+          The moment when my idea turns into reality is the best moment of my
+          life!
         </p>
         <p>
-          I'm now located in Vancouver yet always open to any kind of
-          opportunity to contribute, see and experience creative solutions.
+          I'm currently located in Vancouver yet always open to any kind of
+          opportunity to contribute to software solutions that makes the world
+          the better place.
         </p>
         <p>
           Are you looking for somebody who can make something amazing with? You
-          are in the right place. Feel free to send a message me!
+          are in the right place. Please feel free to email or message me. I
+          would love to hear about your idea!
         </p>
       </FadeInSection>
       <SlashSpace />
